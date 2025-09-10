a5ba3bbcbdb5a3a1d14a4c00f010baa7ab82e625
Mahmut TEKYILDIZ <mahmut.tekyildiz@fairteknoloji.com.tr>
2025-09-10 21:16:13 +0300

    Add initial Docker setup and LoadTester project files

---
+++ LoadTester/Program.cs
@@ -0,0 +1,28 @@
+﻿using NBomber.CSharp;
+using System.Net.Http;
+
+var scenario = Scenario.Create("api_test", async context =>
+    {
+        // Step: API çağrısı
+        var step = await Step.Run("get_post", context, async () =>
+        {
+            using var client = new HttpClient();
+            var res = await client.GetAsync("https://apigw.trendyol.com/discovery-web-accountgw-service/api/locations/cities?culture=tr-TR&storefrontId=1&channelId=1", context.ScenarioCancellationToken);
+
+            if (res.IsSuccessStatusCode)
+                return Response.Ok(statusCode: ((int)res.StatusCode).ToString());
+
+            return Response.Fail(statusCode: ((int)res.StatusCode).ToString());
+        });
+
+        return Response.Ok();
+    })
+    .WithLoadSimulations(
+        // 10 saniyede 10 eşzamanlı kullanıcıya rampa
+        Simulation.RampingConstant(copies: 10, during: TimeSpan.FromSeconds(10))
+    );
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .WithReportingInterval(TimeSpan.FromSeconds(5)) // v6’da min 5 sn
+    .Run();
